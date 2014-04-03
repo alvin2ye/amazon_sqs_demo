2ab32170b3bbcd48ea500db41dc6fa947287b38a
alvin2ye <alvin.ye.cn@gmail.com>
2014-04-03 17:11:02 +0800

    fix

--- receive_message.rb
+++ receive_message.rb
@@ -6,7 +6,7 @@ config = YAML.load_file "config.yml"
 
 sqs = AWS::SQS.new(
   :access_key_id => config["aws"]["access_key_id"],
-  :secret_access_key => config["aws"]["secret_access_key"]
+  :secret_access_key => config["aws"]["secret_access_key"])
 
 url = "https://sqs.ap-northeast-1.amazonaws.com/335799408313/baodao"
 baodao_queue = sqs.queues[url]
