c19e93994289ac6de5fcee51d42c6e5dedeb6454
alvin2ye <alvin.ye.cn@gmail.com>
2014-04-04 15:02:45 +0800

    move url to config

--- receive_message.rb
+++ receive_message.rb
@@ -8,11 +8,10 @@ sqs = AWS::SQS.new(
   :access_key_id => config["aws"]["access_key_id"],
   :secret_access_key => config["aws"]["secret_access_key"])
 
-url = "https://sqs.ap-northeast-1.amazonaws.com/335799408313/baodao"
-baodao_queue = sqs.queues[url]
+queue = sqs.queues[config["aws"]["url"]]
 
 while 1 do
-  if m =  baodao_queue.receive_message
+  if m =  queue.receive_message
     p [m.id, m.body]
   end
   sleep 1
--- send_message.rb
+++ send_message.rb
@@ -8,9 +8,8 @@ sqs = AWS::SQS.new(
   :access_key_id => config["aws"]["access_key_id"],
   :secret_access_key => config["aws"]["secret_access_key"])
 
-url = "https://sqs.ap-northeast-1.amazonaws.com/335799408313/baodao"
-baodao_queue = sqs.queues[url]
+queue = sqs.queues[config["aws"]["url"]]
 
-5.times {
-  baodao_queue.send_message((Time.now.to_f * 10000).to_s)
+2.times {
+  queue.send_message((Time.now.to_f * 10000).to_s)
 }
